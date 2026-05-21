9995601ae8786d5d22a1e2ea31c53102a03dfcc1
legenddiboxe2-collab <legenddiboxe2@gmail.com>
2026-05-21 10:00:14 +0545

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 URLS_LIKE = {"IND":"https://client.ind.freefiremobile.com/LikeProfile",
             "BR":"https://client.us.freefiremobile.com/LikeProfile",
             "US":"https://client.us.freefiremobile.com/LikeProfile",
-            "SAC":"https://client.us.freefiremobile.com/LikeProfile",
+            "BD":"https://client.us.freefiremobile.com/LikeProfile",
             "NA":"https://client.us.freefiremobile.com/LikeProfile"}
 
 
 URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
             "BR":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "US":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
-            "SAC":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+            "BD":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "NA":"https://client.us.freefiremobile.com/GetPlayerPersonalShow"}
 
 
@@ -18,5 +18,5 @@ URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
 FILES = {"IND":"token_ind.json",
              "BR":"token_br.json",
              "US":"token_br.json",
-             "SAC":"token_br.json",
+             "BD":"token_bd.json",
              "NA":"token_br.json"}
